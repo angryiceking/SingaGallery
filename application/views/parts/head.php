98701b3eb514a45164c5a03a6114100fcca5c7bf
me-carl <c.almayda09@gmail.com>
2018-07-10 09:28:36 +0800

    Merge UI to CI

--- application/views/parts/head.php
+++ application/views/parts/head.php
@@ -1,21 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>My Art Asia Circle</title>
+
+  <!-- meta -->
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- <link rel="icon" href="../images/logobilog.png" type="image" /> -->
 
-  <title> HomePage </title><!-- start: Css -->
-  <!-- <link rel="stylesheet" type="text/css" href="../../asset/css/bootstrap.min.css"> -->
+  <!-- css -->
+  <link rel="stylesheet" href="build/css/bootstrap.min.css">
+  <link rel="stylesheet" href="build/css/ionicons.min.css">
+  <link rel="stylesheet" href="build/css/pace.css">
+  <link rel="stylesheet" href="build/css/custom.css">
+  <link rel="stylesheet" href="build/css/carousel.css">
 
-  <!-- plugins -->
-  <!-- <link rel="stylesheet" type="text/css" href="../../asset/css/plugins/font-awesome.min.css"/>
-  <link rel="stylesheet" type="text/css" href="../../asset/css/plugins/datatables.bootstrap.min.css"/>
-  <link rel="stylesheet" type="text/css" href="../../asset/css/plugins/animate.min.css"/>
-  <link href="../../asset/css/style.css" rel="stylesheet"> -->
-  <script type="text/javascript" src="build/js/jquery.min.js"></script>
+  <!-- js -->
+  <script src="build/js/jquery-2.1.3.min.js"></script>
+  <script src="build/js/bootstrap.min.js"></script>
+  <script src="build/js/pace.min.js"></script>
+  <script src="build/js/modernizr.custom.js"></script>
+
+  <link rel="stylesheet" href="build/css/pace.css">
+  <link rel="stylesheet" href="build/css/custom.css">
+  <script src="build/js/carousel.js"></script>
+
 </head>
 <body>
+  <div class="container"> 
+    <header id="site-header">
+      <div class="row">
+        <div class="col-md-4 col-sm-5 col-xs-8">
+          <div class="logo">
+            <h1><a href="index.html"><b>My Art Asia Circle</b></a></h1>
+          </div>
+        </div><!-- col-md-4 -->
+        <div class="col-md-8 col-sm-7 col-xs-4">
+          <nav class="main-nav" role="navigation">
+            <div class="navbar-header">
+              <button type="button" id="trigger-overlay" class="navbar-toggle">
+                <span class="ion-navicon"></span>
+              </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="cl-effect-11"><a href="index.html" data-hover="Home">Home</a></li>
+                <li class="cl-effect-11"><a href="full-width.html" data-hover="Gallery">Gallery</a></li>
+                <li class="cl-effect-11"><a href="full-width.html" data-hover="Blog">Blog</a></li>
+                <li class="cl-effect-11"><a href="events.html" data-hover="Events">Events</a></li>
+                <li class="cl-effect-11"><a href="contact.html" data-hover="Contact">Contact</a></li>
+                <li class="cl-effect-11"><a href="about.html" data-hover="About us">About us</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </nav>
+          <div id="header-search-box">
+            <a id="search-menu" href="#"><span id="search-icon" class="ion-ios-search-strong"></span></a>
+            <div id="search-form" class="search-form">
+              <form role="search" method="get" id="searchform" action="#">
+                <input type="search" placeholder="Search" required>
+                <button type="submit"><span class="ion-ios-search-strong"></span></button>
+              </form>       
+            </div>
+          </div>
+        </div><!-- col-md-8 -->
+      </div>
+    </header>
+  </div>
