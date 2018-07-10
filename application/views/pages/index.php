1c50abeac25574caba89cca38a343169272b3fd5
me-carl <c.almayda09@gmail.com>
2018-07-10 17:50:20 +0800

    Added anon visibility on navbar

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -108,3 +108,42 @@
     </div>
   </div>
 </div>
+
+<div class="modal fade" id="login" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-md" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Login your account</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal">
+          <p><h6>Use your account if you want to buy or to post an artwork for sell.</h6></p>
+          <hr>
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="col-sm-6 pull-right" style="padding-left: 2px;">
+                <h6>or <a href="#" class="btn btn-md btn-info">Register</a> for an account.</h6>
+              </div>
+              <div class="col-sm-6 pull-right" style="padding-right: 0px;">
+                <button type="submit" class="btn btn-default pull-right">Sign in</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div>
--- application/views/parts/head.php
+++ application/views/parts/head.php
@@ -49,19 +49,19 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="cl-effect-11"><a href="index.php" data-hover="Home">Home</a></li>
                 <li class="cl-effect-11"><a href="gallery" data-hover="Gallery">Gallery</a></li>
-                <li class="cl-effect-11"><a href="blog" data-hover="Blog">Blog</a></li>
                 <li class="cl-effect-11"><a href="events" data-hover="Events">Events</a></li>
                 <li class="cl-effect-11"><a href="contacts" data-hover="Contact">Contact</a></li>
-                <li class="cl-effect-11"><a href="about-us" data-hover="About us">About</a></li>
+                <li class="cl-effect-11"><a href="about-us" data-hover="About">About</a></li>
+                <?php if ((!$this->session->has_userdata('is_signed') && $this->session->is_signed != TRUE)): ?>
+                  <li class="cl-effect-11"><a href="#" class="btn btn-sm btn-inverse" data-toggle="modal" data-target="#login"><small class="text-danger">Anon</small></a></li>
+                <?php else: ?>
+                  <li class="cl-effect-11"><small><?php echo $this->session->name?></small></li>
+                <?php endif ?>
               </ul>
             </div><!-- /.navbar-collapse -->
           </nav>
           <div id="header-search-box">
-            <?php if ((!$this->session->has_userdata('is_signed') && $this->session->is_signed != TRUE)): ?>
-              <small class="text-danger">Anon</small>
-             <?php else: ?>
-              <small><?php echo $this->session->name?></small>
-            <?php endif ?>
+
           </div>
         </div><!-- col-md-8 -->
       </div>
