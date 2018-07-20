86eced2a162defe9272694003610a85e6e1f5c01
Carl <Carl>
2018-07-20 15:15:10 +0800

    added database sql

--- artasia_db.sql
+++ artasia_db.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2018 at 07:57 AM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 7.1.4
+-- Generation Time: Jul 20, 2018 at 09:14 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,6 +23,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` int(255) NOT NULL,
+  `blog_name` varchar(255) NOT NULL,
+  `blog_author` varchar(255) NOT NULL,
+  `blog_desc` varchar(999) NOT NULL,
+  `blog_created_date` varchar(255) NOT NULL,
+  `blog_img` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `status_date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
@@ -36,6 +51,36 @@ CREATE TABLE `comments` (
   `time_log` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `user_id`, `art_id`, `comment`, `time_log`) VALUES
+(92, 1, 1, 'heyo', '2018-07-19 09:46:43'),
+(93, 1, 1, 'heyo', '2018-07-19 09:46:44'),
+(94, 1, 1, 'nice one dude', '2018-07-19 09:46:50'),
+(95, 1, 1, 'good gallery art, recommended!', '2018-07-19 09:47:02'),
+(96, 2, 2, 'oh nice', '2018-07-19 09:47:10'),
+(97, 2, 2, 'what a nice art', '2018-07-19 09:47:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(255) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `event_desc` varchar(999) NOT NULL,
+  `event_img` varchar(999) NOT NULL,
+  `event_date` varchar(255) NOT NULL,
+  `event_due_date` varchar(255) NOT NULL,
+  `event_author` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `status_date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- --------------------------------------------------------
 
 --
@@ -45,18 +90,29 @@ CREATE TABLE `comments` (
 CREATE TABLE `gallery` (
   `id` int(255) NOT NULL,
   `img` varchar(255) NOT NULL,
-  `artist` varchar(50) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `contact` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `title` varchar(50) NOT NULL,
+  `date_added` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `supply` varchar(255) NOT NULL,
-  `status` varchar(25) NOT NULL,
-  `notes` varchar(255) NOT NULL,
-  `other_info_one` int(255) NOT NULL,
-  `other_info_two` int(255) NOT NULL
+  `size` varchar(255) NOT NULL,
+  `medium` varchar(255) NOT NULL,
+  `price` varchar(25) NOT NULL,
+  `artist` varchar(255) NOT NULL,
+  `status` varchar(500) NOT NULL,
+  `notes` varchar(500) NOT NULL,
+  `rank` varchar(25) NOT NULL,
+  `reference_links` int(11) NOT NULL,
+  `reference_id` int(11) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `img`, `date_added`, `title`, `description`, `size`, `medium`, `price`, `artist`, `status`, `notes`, `rank`, `reference_links`, `reference_id`, `contact`, `email`) VALUES
+(1, 'carl-picasso-lost-for-words.jpg', '', 'Lost for Words', 'The lost of words.. this potrait was finished by the famous Filipino painter Carl Picasso. It symbolizes the lost of words due to unexpected event that makes someone be lost at words', '62cm x 92cm (24 in x 36 in), without frame', 'Oil on Canvas', '$125', 'Carl Picasso', 'on shelf', 'ships from overseas', '1', 0, 0, '', ''),
+(2, 'carl-picasso-unconditional-love.jpg', '', 'Unconditional Love', 'This painting describes that every mother gives unconditional love in any species. The mother dog potrayed are sample of how caring mothers can be', '89cm x 89cm (35 in x 35 in), with frame', 'Mixed media (oil and acrylic on canvas)', '$300', 'Carl Picasso', 'on shelf', 'on local suppliers', '2', 0, 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -65,11 +121,18 @@ CREATE TABLE `gallery` (
 --
 
 CREATE TABLE `likes` (
-  `id` int(124) NOT NULL,
-  `art_id` int(124) NOT NULL,
-  `user_id` int(124) NOT NULL,
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `art_id` int(255) NOT NULL,
   `like_desc` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `user_id`, `art_id`, `like_desc`) VALUES
+(26, 2, 1, '');
 
 -- --------------------------------------------------------
 
@@ -95,16 +158,41 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `nickname`, `email`, `last_login`, `last_logout`, `type`, `img`, `contact_num`) VALUES
-(1, 'carljshra', 'password', 'King', 'c.almayda09@gmail.com', '', '', 'user', '', 0);
+(1, 'carljshra', 'password', 'King', 'c.almayda09@gmail.com', '', '', 'user', '', 0),
+(2, 'administrator', 'administrator', 'Ultimate Administrator', 'admin.email@gmail.com', '', '', 'admin', '', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -118,16 +206,35 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
-
+  MODIFY `id` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
